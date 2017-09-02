9334a585a657472f529a20fb4b2a85ae7a97d174
Tugberk Ugurlu <tugberk@outlook.com>
2017-09-02 21:54:26 +0100

    basic header and layout setup

--- src/Tugberk.Web/Startup.cs
+++ src/Tugberk.Web/Startup.cs
@@ -5,14 +5,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Tugberk.Web
 {
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -22,10 +29,8 @@ namespace Tugberk.Web
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
