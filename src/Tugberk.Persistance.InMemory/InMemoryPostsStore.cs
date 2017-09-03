3fd8b36c1a81cc3d72ea8cadf883a106d77c251a
Tugberk Ugurlu <tugberk@outlook.com>
2017-09-03 15:33:05 +0100

    multiple sample post for in-mem presistance

--- src/Tugberk.Persistance.InMemory/InMemoryPostsStore.cs
+++ src/Tugberk.Persistance.InMemory/InMemoryPostsStore.cs
@@ -14,7 +14,7 @@ namespace Tugberk.Persistance.InMemory
     {
         public Task<PostFindResult> FindApprovedPostById(string id) 
         {
-            var post = GetSamplePost();
+            var post = GetSamplePost1();
             return Task.FromResult(PostFindResult.Success(post));
         }
 
@@ -30,22 +30,25 @@ namespace Tugberk.Persistance.InMemory
 
         public Task<IReadOnlyCollection<Post>> GetLatestApprovedPosts(int skip, int take)
         {
-            var post = GetSamplePost();
-            return Task.FromResult<IReadOnlyCollection<Post>>(new [] { post });
+            return Task.FromResult<IReadOnlyCollection<Post>>(new [] 
+            { 
+                GetSamplePost1(),
+                GetSamplePost2()
+            });
         }
 
-        private static Post GetSamplePost()
+        private static Post GetSamplePost1()
         {
-            var location = Assembly.GetEntryAssembly().Location;
-            var directory = Path.GetDirectoryName(location);
-            var samplePostPath = Path.Combine(directory, "sampe-post-html.txt");
+            const string path = "sample-post-html-1.txt";
+            string content = GetPostContent(path);
+
             var samplePost = new Post();
             samplePost.Id = "ydy982d";
             samplePost.Title = "Defining What Good Looks Like for a Software Engineer";
             samplePost.Abstract = "What does good look like for a software engineer? This is a question you might be asking frequently to yourself and I tried to share my thoughts on the topic with this blog post.";
             samplePost.Language = "en-US";
             samplePost.Format = PostFormat.Html;
-            samplePost.Content = File.ReadAllText(samplePostPath, Encoding.UTF8);
+            samplePost.Content = content;
             var user = new User
             {
                 Id = "djidiweh",
@@ -123,6 +126,148 @@ namespace Tugberk.Persistance.InMemory
             };
 
             return samplePost;
+        }
+
+        private static Post GetSamplePost2()
+        {
+            const string path = "sample-post-html-2.txt";
+            string content = GetPostContent(path);
+
+            var samplePost = new Post();
+            samplePost.Id = "y9h9huj9u";
+            samplePost.Title = "ASP.NET Core Authentication in a Load Balanced Environment with HAProxy and Redis";
+            samplePost.Abstract = "Token based authentication is a fairly common way of authenticating a user for an HTTP application. However, handling this in a load balanced environment has always involved extra caring. In this post, I will show you how this is handled in ASP.NET Core by demonstrating it with HAProxy and Redis through the help of Docker.";
+            samplePost.Language = "en-US";
+            samplePost.Format = PostFormat.Html;
+            samplePost.Content = content;
+            var user = new User
+            {
+                Id = "djidiweh",
+                Name = "Tugberk Ugurlu"
+            };
+
+            samplePost.Authors = (new[] { user });
+
+            samplePost.CommentStatusActions = new[]
+            {
+                new CommentStatusActionRecord
+                {
+                    Status = CommentableStatus.Enabled,
+                    RecordedBy = user,
+                    RecordedOn = DateTime.UtcNow
+                }
+            };
+
+            samplePost.ApprovaleStatusActions = new[]
+            {
+                new ApprovalStatusActionRecord
+                {
+                    Status = ApprovalStatus.Approved,
+                    RecordedBy = user,
+                    RecordedOn = DateTime.UtcNow
+                }
+            };
+
+            samplePost.CreationRecord = new ChangeRecord
+            {
+                RecordedBy = user,
+                RecordedOn = DateTime.UtcNow,
+                IpAddress = "127.0.0.1"
+            };
+
+            samplePost.Slugs = new[]
+            {
+                new Slug
+                {
+                    Path = "asp-net-core-authentication-in-a-load-balanced-environment-with-haproxy-and-redis",
+                    IsDefault = true,
+                    CreatedOn = DateTime.UtcNow
+                }
+            };
+
+            samplePost.Tags = new[]
+            {
+                new Tag
+                {
+                    Name = "ASP.NET Core",
+                    Slugs = new []
+                    {
+                        new Slug
+                        {
+                            Path = "asp-net-core",
+                            IsDefault = true,
+                            CreatedOn = DateTime.UtcNow
+                        }
+                    }
+                },
+
+                new Tag
+                {
+                    Name = "ASP.NET",
+                    Slugs = new []
+                    {
+                        new Slug
+                        {
+                            Path = "asp-net",
+                            IsDefault = true,
+                            CreatedOn = DateTime.UtcNow
+                        }
+                    }
+                },
+
+                new Tag
+                {
+                    Name = "Security",
+                    Slugs = new []
+                    {
+                        new Slug
+                        {
+                            Path = "security",
+                            IsDefault = true,
+                            CreatedOn = DateTime.UtcNow
+                        }
+                    }
+                },
+
+                new Tag
+                {
+                    Name = "HTTP",
+                    Slugs = new []
+                    {
+                        new Slug
+                        {
+                            Path = "http",
+                            IsDefault = true,
+                            CreatedOn = DateTime.UtcNow
+                        }
+                    }
+                },
+
+                new Tag
+                {
+                    Name = "Docker",
+                    Slugs = new []
+                    {
+                        new Slug
+                        {
+                            Path = "docker",
+                            IsDefault = true,
+                            CreatedOn = DateTime.UtcNow
+                        }
+                    }
+                }
+            };
+
+            return samplePost;
+        }
+
+        private static string GetPostContent(string path)
+        {
+            var location = Assembly.GetEntryAssembly().Location;
+            var directory = Path.GetDirectoryName(location);
+            var fullPath = Path.Combine(directory, path);
+
+            return File.ReadAllText(fullPath, Encoding.UTF8);
         }
     }
 }
