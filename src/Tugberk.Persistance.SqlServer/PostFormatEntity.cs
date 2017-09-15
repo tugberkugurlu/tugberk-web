066269581e126b2bda21bb280d3e5d275f405659
Tugberk Ugurlu <tugberk@outlook.com>
2017-09-15 23:53:45 +0100

    add PostFormatEntityExtensions

--- src/Tugberk.Persistance.SqlServer/PostFormatEntity.cs
+++ src/Tugberk.Persistance.SqlServer/PostFormatEntity.cs
@@ -1,3 +1,6 @@
+using System;
+using Tugberk.Domain;
+
 namespace Tugberk.Persistance.SqlServer
 {
     public enum PostFormatEntity 
@@ -5,5 +8,26 @@ namespace Tugberk.Persistance.SqlServer
         PlainText = 1,
         Html = 2,
         Markdown = 3
+    }
+
+    public static class PostFormatEntityExtensions 
+    {
+        public static PostFormat ToDomainModel(this PostFormatEntity postFormatEntity) 
+        {
+            switch (postFormatEntity)
+            {
+                case PostFormatEntity.Html:
+                    return PostFormat.Html;
+
+                case PostFormatEntity.Markdown:
+                    return PostFormat.Markdown;
+
+                case PostFormatEntity.PlainText:
+                    return PostFormat.PlainText;
+
+                default:
+                    throw new NotSupportedException();
+            }
+        }
     }
 }
