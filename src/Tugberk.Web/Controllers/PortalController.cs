65dd65951364fd204e74f04a7102d2ef7cafa3af
Tugberk Ugurlu <tugberk@outlook.com>
2017-09-16 13:04:32 +0100

    server side basics of image upload, failing since antiforgerytoken is not set

--- src/Tugberk.Web/Controllers/PortalController.cs
+++ src/Tugberk.Web/Controllers/PortalController.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Tugberk.Web.Controllers
@@ -12,5 +15,16 @@ namespace Tugberk.Web.Controllers
 
         [HttpGet("posts/{postId}")]
         public IActionResult EditPost(string postId) => View();
+
+        [ValidateAntiForgeryToken]
+        [HttpPost("images/upload")]
+        public async Task<IActionResult> UploadImage(IEnumerable<IFormFile> files)
+        {
+            await Task.Delay(2000);
+            return Json(new 
+            { 
+                ImageUrl = "https://tugberkugurlu.blob.core.windows.net/bloggyimages/640px-Coding_Shots_Annual_Plan_high_res-5.jpg" 
+            });
+        }
     }
 }
