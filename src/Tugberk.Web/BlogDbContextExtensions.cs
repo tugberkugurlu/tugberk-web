c1b41cf326e87b17654179cc3cdee8a752c58da0
Tugberk Ugurlu <tugberk@outlook.com>
2017-09-17 23:44:24 +0100

    seed name and surname

--- src/Tugberk.Web/BlogDbContextExtensions.cs
+++ src/Tugberk.Web/BlogDbContextExtensions.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Tugberk.Persistance.SqlServer;
@@ -17,6 +18,11 @@ namespace Tugberk.Web
                 };
 
                 userManager.CreateAsync(defaultUser, "P@asW0rd").Wait();
+                userManager.AddClaimsAsync(defaultUser, new [] 
+                {
+                    new Claim(ClaimTypes.Name, "Default"),
+                    new Claim(ClaimTypes.Surname, "Author")
+                }).Wait();
             }
         }
     }
