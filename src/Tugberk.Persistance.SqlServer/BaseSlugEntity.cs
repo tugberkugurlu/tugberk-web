5dd9ff7e380b1e2f9f4258d959ca7776b29273f9
Tugberk Ugurlu <tugberk@outlook.com>
2017-09-20 19:30:27 +0100

    add BaseSlugEntity

---
+++ src/Tugberk.Persistance.SqlServer/BaseSlugEntity.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Tugberk.Persistance.SqlServer
+{
+    public abstract class BaseSlugEntity 
+    {
+        [Key]
+        public string Path { get; set; }
+        public bool IsDefault { get; set; }
+        public DateTime CreatedOnUtc { get; set; }
+
+        [Required]
+        public IdentityUser CreatedBy { get; set; }
+    }
+}
