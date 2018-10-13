7746ec43aa122bd02e60440213350c7080f5efc1
Tugberk Ugurlu <tugberk@outlook.com>
2018-10-13 22:45:43 +0100

    tag equality to be made case incensitive

--- src/Tugberk.Web.OldBlogMigrator/TagEquality.cs
+++ src/Tugberk.Web.OldBlogMigrator/TagEquality.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Bloggy.Domain.Entities;
 
 namespace Tugberk.Web.OldBlogMigrator
 {
     internal class TagEquality : IEqualityComparer<Tag>
     {
-        public bool Equals(Tag x, Tag y) => x.Name.Equals(y.Name);
-        public int GetHashCode(Tag obj) => obj.Name.GetHashCode();
+        public bool Equals(Tag x, Tag y) => x.Name.Equals(y.Name, StringComparison.InvariantCultureIgnoreCase);
+        public int GetHashCode(Tag obj) => obj.Name.ToLowerInvariant().GetHashCode();
     }
 }
