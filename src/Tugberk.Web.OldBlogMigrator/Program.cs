a249c0394412d1ad75e5f5fd8508b945fd43c1c5
Tugberk Ugurlu <tugberk@outlook.com>
2018-10-13 22:15:30 +0100

    first pass on the migration code, should work as expected. We will find the bugs soon!

--- src/Tugberk.Web.OldBlogMigrator/Program.cs
+++ src/Tugberk.Web.OldBlogMigrator/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using Bloggy.Domain.Entities;
@@ -10,15 +9,10 @@ namespace Tugberk.Web.OldBlogMigrator
 {
     class Program
     {
-        private class TagEquality : IEqualityComparer<Tag>
-        {
-            public bool Equals(Tag x, Tag y) => x.Name.Equals(y.Name);
-            public int GetHashCode(Tag obj) => obj.Name.GetHashCode();
-        }
-
         static void Main(string[] args)
         {
             var connStr = args[0];
+            var userId = args[1];
 
             using (IDocumentStore store = RetrieveDocumentStore())
             using (IDocumentSession ses = store.OpenSession())
@@ -26,19 +20,107 @@ namespace Tugberk.Web.OldBlogMigrator
             {
                 conn.Open();
                 ses.Advanced.MaxNumberOfRequestsPerSession = int.MaxValue;
-                var userId = Guid.NewGuid().ToString();
 
                 var allPosts = ses.Query<BlogPost>().Take(Int32.MaxValue).Where(x => x.IsApproved).ToList();
                 var allTags = allPosts.SelectMany(x => x.Tags).Distinct(new TagEquality());
-
+                
                 foreach (var tag in allTags)
                 {
+                    Console.WriteLine($"Inserting Tag '{tag.Name}'");
                     InsertTag(conn, tag, userId);
                 }
 
                 foreach (var blogPost in allPosts)
                 {
-                    Console.WriteLine($"Found '{blogPost.Title}' post, written @ {blogPost.CreatedOn}");
+                    Console.WriteLine($"Inserting '{blogPost.Title}' post, written @ {blogPost.CreatedOn}");
+                    InsertPost(conn, blogPost, userId);
+                }
+            }
+        }
+
+        private static void InsertPost(SqlConnection conn, BlogPost blogPost, string userId)
+        {
+            var blogPostNewId = Guid.NewGuid().ToString();
+            using (var cmd = new SqlCommand(@"INSERT INTO [dbo].[Posts]
+           ([Id]
+           ,[Abstract]
+           ,[Content]
+           ,[CreatedById]
+           ,[CreatedOnUtc]
+           ,[CreationIpAddress]
+           ,[Format]
+           ,[Language]
+           ,[Title])
+     VALUES (@Id, @Abstract, @Content, @CreatedById, @CreatedOnUtc, @CreationIpAddress, @Format, @Language, @Title)",
+                conn))
+            {
+                // TODO: Old Identifier?
+                // TODO: Last updated date?
+                
+                cmd.Parameters.AddWithValue("Id", blogPostNewId);
+                cmd.Parameters.AddWithValue("Abstract", blogPost.BriefInfo);
+                cmd.Parameters.AddWithValue("Content", blogPost.Content);
+                cmd.Parameters.AddWithValue("CreatedById", userId);
+                cmd.Parameters.AddWithValue("CreatedOnUtc", blogPost.CreatedOn.UtcDateTime);
+                cmd.Parameters.AddWithValue("CreationIpAddress", blogPost.CreationIp);
+                cmd.Parameters.AddWithValue("Format", 2);
+                cmd.Parameters.AddWithValue("Language", "en-US");
+                cmd.Parameters.AddWithValue("Title", blogPost.Title);
+
+                cmd.ExecuteNonQuery();
+            }
+
+            foreach (var blogPostTag in blogPost.Tags)
+            {
+                using (var cmd = new SqlCommand(@"INSERT INTO [dbo].[PostTagEntity]
+           ([PostId]
+           ,[TagName])
+     VALUES (@PostId, @TagName)", conn))
+                {
+                    cmd.Parameters.AddWithValue("PostId", blogPostNewId);
+                    cmd.Parameters.AddWithValue("TagName", blogPostTag.Name);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            foreach (var blogPostSlug in blogPost.Slugs)
+            {
+                using (var cmd = new SqlCommand(@"INSERT INTO [dbo].[PostSlugEntity]
+           ([Path]
+           ,[CreatedById]
+           ,[CreatedOnUtc]
+           ,[IsDefault]
+           ,[OwnedById])
+     VALUES (@Path, @CreatedById, @CreatedOnUtc, @IsDefault, @OwnedById)", conn))
+                {
+                    cmd.Parameters.AddWithValue("Path", blogPostSlug.Path);
+                    cmd.Parameters.AddWithValue("CreatedById", userId);
+                    cmd.Parameters.AddWithValue("CreatedOnUtc", blogPostSlug.CreatedOn.UtcDateTime);
+                    cmd.Parameters.AddWithValue("IsDefault", blogPostSlug.IsDefault);
+                    cmd.Parameters.AddWithValue("OwnedById", blogPostNewId);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            if (blogPost.IsApproved)
+            {
+                using (var cmd = new SqlCommand(@"INSERT INTO [dbo].[PostApprovalStatusActionEntity]
+           ([Id]
+           ,[PostId]
+           ,[RecordedById]
+           ,[RecordedOnUtc]
+           ,[Status])
+     VALUES (@Id, @PostId, @RecordedById, @RecordedOnUtc, @Status)", conn))
+                {
+                    cmd.Parameters.AddWithValue("Id", Guid.NewGuid().ToString());
+                    cmd.Parameters.AddWithValue("PostId", blogPostNewId);
+                    cmd.Parameters.AddWithValue("RecordedById", userId);
+                    cmd.Parameters.AddWithValue("RecordedOnUtc", DateTime.UtcNow);
+                    cmd.Parameters.AddWithValue("Status", 1);
+
+                    cmd.ExecuteNonQuery();
                 }
             }
         }
@@ -57,6 +139,8 @@ namespace Tugberk.Web.OldBlogMigrator
                 cmd.Parameters.AddWithValue("CreatedOnUtc", DateTime.UtcNow);
                 cmd.Parameters.AddWithValue("CreationIpAddress", "127.0.0.1");
                 cmd.Parameters.AddWithValue("Slug", tag.Slug);
+
+                cmd.ExecuteNonQuery();
             }
         }
 
---
+++ src/Tugberk.Web.OldBlogMigrator/TagEquality.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Bloggy.Domain.Entities;
+
+namespace Tugberk.Web.OldBlogMigrator
+{
+    internal class TagEquality : IEqualityComparer<Tag>
+    {
+        public bool Equals(Tag x, Tag y) => x.Name.Equals(y.Name);
+        public int GetHashCode(Tag obj) => obj.Name.GetHashCode();
+    }
+}
