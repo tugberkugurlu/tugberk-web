16fdcfc3b457fca48c4dddd580d79ce4e47f192d
Tugberk Ugurlu <tugberk@outlook.com>
2018-10-13 18:53:51 +0100

    prefix environment variable config values to TUGBERKWEB_

--- src/Tugberk.Web/Program.cs
+++ src/Tugberk.Web/Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace Tugberk.Web
 {
@@ -9,6 +11,20 @@ namespace Tugberk.Web
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, builder) =>
+                {
+                    // see https://github.com/aspnet/MetaPackages/blob/2.1.4/src/Microsoft.AspNetCore/WebHost.cs#L150
+                    var env = hostingContext.HostingEnvironment;
+                    builder.SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                        .AddEnvironmentVariables("TUGBERKWEB_");
+
+                    if (args != null)
+                    {
+                        builder.AddCommandLine(args);
+                    }
+                })
                 .UseStartup<Startup>()
                 .Build();
     }
